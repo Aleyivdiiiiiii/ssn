abb5ce76e33c6720be8eb4d9f5a2f9a72624c604
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2022-03-09 02:31:39 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Mehmetbaba55/MusicTR",
+    "https://github.com/MehmetAtes21/TR",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
